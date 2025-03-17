f7b744bb9d048f7f5cbfbaa5fa1a14ba3f468ed4
paprisko <jackmchussein965@gmail.com>
2025-03-17 09:12:42 +0300

    phase-1-array-filter-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+// 1: findMatching
+function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+  }
+  
+  // 2: fuzzyMatch
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+  }
+  
+  // 3: matchName
+  function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name === query);
+  }
+  
